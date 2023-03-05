fadaf646835e8bcf64aff7eba64344fe134a6c0a
Little Tomato <huyyhoangg150203@hotmail.com>
2023-03-05 15:17:32 +0700

    init 2

--- src/main/java/me/hyhon/leggyspawncontroller/leggyspawncontroller/config/MainConfig.java
+++ src/main/java/me/hyhon/leggyspawncontroller/leggyspawncontroller/config/MainConfig.java
@@ -3,6 +3,8 @@ package me.hyhon.leggyspawncontroller.leggyspawncontroller.config;
 import com.pixelmonmod.pixelmon.Pixelmon;
 import com.pixelmonmod.pixelmon.api.pokemon.PokemonSpec;
 import com.pixelmonmod.pixelmon.enums.EnumSpecies;
+import info.pixelmon.repack.ninja.leaping.configurate.ConfigurationNode;
+import info.pixelmon.repack.ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import io.leangen.geantyref.TypeToken;
 import me.hyhon.leggyspawncontroller.leggyspawncontroller.serializers.BigDecimalSerializer;
 import me.hyhon.leggyspawncontroller.leggyspawncontroller.serializers.TextComponentStringSerializer;
@@ -16,16 +18,19 @@ import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 import org.spongepowered.configurate.objectmapping.meta.Setting;
 
 import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.Map;
 
 @ConfigSerializable
 public class MainConfig {
 
 	private File configFile;
-	private Configuration config;
+	private ConfigurationNode rootNode;
+	private HoconConfigurationLoader loader;
 	private HashMap<String, Long> configMap;
 
 	public TextComponentString getReloadMessage() {
@@ -43,54 +48,52 @@ public class MainConfig {
 		} catch (Exception e){}
 	}
 
-	public Property getProperty(String categoryName, String propertyName, int defaultValue, String comment) {
-		Property prop = config.get(categoryName, propertyName, defaultValue, comment);
-		if (config.hasChanged()) {
-			config.save();
-		}
-		return prop;
-	}
 
-	private void loadConfig() {
-		config = new Configuration(configFile);
-		config.load();
+	private void loadConfig() throws IOException {
+		loader = HoconConfigurationLoader.builder().setFile(configFile).build();
+		rootNode = loader.load();
+
+
+
 		if (configFile.exists() && configFile.length() > 0) {
 			//FMLLog.log(Level.WARN, "111111111111111111111111111111");
 
 
-			if (config.getCategory("delay").getValues().keySet().size() <= 0) {
-
-				Property myProperty = getProperty("delay", "Rayquaza", 100, "");
-				myProperty.set(100);
-
-				if (config.hasChanged()) {
-					config.save();
-				}
-			} else {
-				for (String categoryName : config.getCategoryNames()) {
-					for (String propertyName : config.getCategory(categoryName).getValues().keySet()) {
-						long propertyValue = config.get(categoryName, propertyName, 0).getInt();
-						configMap.put(propertyName, propertyValue);
+			if (rootNode.getNode("delay").isVirtual()) {
+				rootNode.getNode("delay").getNode("Rayquaza").setValue(100);
+				loader.save(rootNode);
+			}
+			else {
+				if (rootNode.getNode("delay").hasMapChildren()) {
+					Map<Object, ? extends ConfigurationNode> childrenMap = rootNode.getNode("delay").getChildrenMap();
+					for (Map.Entry<Object, ? extends ConfigurationNode> entry : childrenMap.entrySet()) {
+						if (entry.getKey() != null && entry.getValue() != null) {
+							configMap.put((String) entry.getKey(), entry.getValue().getLong());
+							//FMLLog.log(Level.WARN, entry.getKey() + " " + entry.getValue().getLong());
+						}
 					}
 				}
 			}
 		}
-		config.save();
+		rootNode = loader.load();
 
-	}
-
-	public Configuration getConfiguration() {
-		return config;
 	}
 
 	public void reloadConfig() {
-		config.load();
-		configMap.clear();
-		loadConfig();
+		try {
+			rootNode = loader.load();
+			saveConfig();
+			configMap.clear();
+			loadConfig();
+		} catch (IOException e) {
+		}
 	}
 
 	public void saveConfig() {
-		config.save();
+		try {
+			loader.save(rootNode);
+		} catch (IOException e)  {
+		}
 	}
 
 	public HashMap<String, Long> getConfigMap() {
