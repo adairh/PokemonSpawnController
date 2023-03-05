91271d6a1b27812d2110d123ee080ba3271ecb5d
Little Tomato <huyyhoangg150203@hotmail.com>
2023-03-05 22:28:30 +0700

    beautify

--- src/main/java/me/hyhon/leggyspawncontroller/leggyspawncontroller/events/Spawning.java
+++ src/main/java/me/hyhon/leggyspawncontroller/leggyspawncontroller/events/Spawning.java
@@ -1,76 +1,81 @@
 package me.hyhon.leggyspawncontroller.leggyspawncontroller.events;
 
 import com.pixelmonmod.pixelmon.api.events.spawning.LegendarySpawnEvent;
+import com.pixelmonmod.pixelmon.api.events.spawning.SpawnEvent;
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+import com.pixelmonmod.pixelmon.api.spawning.SpawnAction;
+import com.pixelmonmod.pixelmon.api.spawning.archetypes.entities.pokemon.SpawnActionPokemon;
+import com.pixelmonmod.pixelmon.entities.pixelmon.EntityPixelmon;
 import me.hyhon.leggyspawncontroller.leggyspawncontroller.LeggySpawnController;
 import me.hyhon.leggyspawncontroller.leggyspawncontroller.config.MainConfig;
 import me.hyhon.leggyspawncontroller.leggyspawncontroller.Manager.StorageManager;
+import net.minecraft.entity.Entity;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Optional;
 
 public class Spawning {
 
     @SubscribeEvent
-    public void onLegendarySpawn(LegendarySpawnEvent.DoSpawn event) {
+    public void onLegendarySpawn(SpawnEvent event) {
         StorageManager sm = LeggySpawnController.storageManager;
         MainConfig mc = LeggySpawnController.mainConfig;
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String sdate = sdf.format(new Date());
-        if (sm.getMap() != null) {
-            if (sm.getMap().size() > 0) {
-                if (!sm.getMap().containsKey(event.getLegendary().name)) {
-                    sm.commitAndPush(event.getLegendary().name, sdate);
-                    //FMLLog.log(Level.WARN, "aaaaaaaaaaaaaaaaaa");
-                    return;
-                } else {
-                    try {
-                        Date date = sdf.parse(sm.getMap().get(event.getLegendary().name));
-                        if (mc.getConfigMap() != null) {
-                            if (mc.getConfigMap().size() > 0) {
-                                if (mc.getConfigMap().containsKey(event.getLegendary().name)) {
-                                    long sec = mc.getConfigMap().get(event.getLegendary().name);
-                                    if (new Date().getTime() - date.getTime() >= sec * 1000) {
-                                        //FMLLog.log(Level.WARN, "bbbbbbbbbbbbbbbbbb");
-                                        sm.getMap().remove(event.getLegendary().name);
-                                        sm.pushData();
-                                        sm.commitAndPush(event.getLegendary().name, sdate);
-                                        return;
-                                    }
-                                } else {
-                                    //FMLLog.log(Level.WARN, "ccccccccccccccccccccc");
-                                    sm.commitAndPush(event.getLegendary().name, sdate);
-                                    return;
-                                }
-                            } else {
-                                //FMLLog.log(Level.WARN, "dddddddddddddddddd");
-                                sm.commitAndPush(event.getLegendary().name, sdate);
-                                return;
-                            }
-                        } else {
-                            //FMLLog.log(Level.WARN, "eeeeeeeeeeeeeeeeee");
-                            sm.commitAndPush(event.getLegendary().name, sdate);
-                            return;
-                        }
-                    } catch (ParseException e) {
-                        //FMLLog.log(Level.WARN, "ffffffffffffffffff");
-                        sm.commitAndPush(event.getLegendary().name, sdate);
-                        return;
-                    }
-                }
-            } else {
-                //FMLLog.log(Level.WARN, "gggggggggggggggggg");
-                sm.commitAndPush(event.getLegendary().name, sdate);
-                return;
-            }
-        } else {
-            //FMLLog.log(Level.WARN, "hhhhhhhhhhhhhhhhh");
-            sm.commitAndPush(event.getLegendary().name, sdate);
+
+        SpawnAction<? extends Entity> action = event.action;
+
+        if (!(action instanceof SpawnActionPokemon)) {
             return;
         }
-        //FMLLog.log(Level.WARN, "iiiiiiiiiiiiiiiiiiiiii");
+
+        EntityPixelmon pixelmon = ((SpawnActionPokemon)action).getOrCreateEntity();
+
+        /*if (sm.getMap() == null) {
+            sm.commitAndPush(pixelmon.getName(), sdate);
+            return;
+        }
+        if (sm.getMap().size() <= 0) {
+            sm.commitAndPush(pixelmon.getName(), sdate);
+            return;
+        }
+        if (!sm.getMap().containsKey(pixelmon.getName())) {
+            sm.commitAndPush(pixelmon.getName(), sdate);
+            return;
+        }
+        if (mc.getConfigMap() == null) {
+            sm.commitAndPush(pixelmon.getName(), sdate);
+            return;
+        }
+
+        if (mc.getConfigMap().size() <= 0) {
+            sm.commitAndPush(pixelmon.getName(), sdate);
+            return;
+        }
+        if (!mc.getConfigMap().containsKey(pixelmon.getName())) {
+            sm.commitAndPush(pixelmon.getName(), sdate);
+            return;
+        }*/
+        try {
+            Date date = sdf.parse(sm.getMap().get(pixelmon.getName()));
+            long sec = mc.getConfigMap().get(pixelmon.getName());
+            if (new Date().getTime() - date.getTime() >= sec * 1000) {
+                sm.getMap().remove(pixelmon.getName());
+                sm.pushData();
+                sm.commitAndPush(pixelmon.getName(), sdate);
+                return;
+            }
+        } catch (Exception e) {
+            sm.commitAndPush(pixelmon.getName(), sdate);
+            return;
+        }
+
+
+
         event.setCanceled(true);
 
     }
